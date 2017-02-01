a0e45f727bfded18a915653e89e3049c18d98564
rputra <rhpmail@gmail.com>
2017-02-01 21:49:49 +0800

    similarity changes

--- spacyapp/views.py
+++ spacyapp/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 import spacy
 
 en_nlp = spacy.load('en')
-commands = "start"
+commands = "start stop left right up down"
 
 def postagger(request):
     message = request.GET.get('s')
@@ -18,10 +18,16 @@ def similarity(request):
     en_doc = en_nlp(message) # text from client
     cmd = en_nlp(commands)   # list of pre-defined commands
     resp=""
+    
     for token in en_doc:
         if(token.pos_ == 'VERB'):
-            resp = resp + token.text + ' ' + token.pos_ + ' ' + str(token.similarity(cmd[0])) + '<br>'
-            
+                for cmdtoken in cmd:
+                        print(cmdtoken.text)
+                    #if(token.similarity(cmdtoken) >= 0.6):
+                        resp = resp + token.text + ' ' + token.pos_ + ' ' + str(token.similarity(cmdtoken)) + ' = ' + cmdtoken.text + '<br>'
+                        #break
+                #break
+
     if(resp==""):
         resp = "Sorry, I dont understand it"
     
