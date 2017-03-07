0b5179e8e10a7bb974f48cf78d52d22d845e247d
rputra <rhpmail@gmail.com>
2017-03-08 00:52:48 +0800

    created piserver folder and venv

--- multiwii/test-arm-disarm.py
+++ multiwii/test-arm-disarm.py
@@ -17,7 +17,7 @@ import time
 if __name__ == "__main__":
 
     #board = MultiWii("/dev/tty.usbserial-AM016WP4")
-    board = MultiWii("/dev/ttyUSB0")
+    board = MultiWii("/dev/tty.SLAB_USBtoUART")
     try:
         board.arm()
         print "Board is armed now!"
---
+++ piserver/droneserver.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+"""droneserver.py: Handles incoming socket request and send command to FC using Multiwii Serial Protocol."""
+
+__author__ = "Ricky Putra"
+__copyright__ = "Copyright 2017"
+
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Ricky Putra"
+__email__ = "rhpmail@gmail.com"
+__status__ = "Development"
+
+import tornado.web
+import tornado.websocket
+import tornado.httpserver
+import tornado.ioloop
+ 
+ 
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+    
+    def open(self):
+        pass
+ 
+    def on_message(self, message):
+        self.write_message(u"Your message was: " + message)
+ 
+    def on_close(self):
+        pass
+ 
+ 
+class IndexPageHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+ 
+ 
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/', IndexPageHandler),
+            (r'/websocket', WebSocketHandler)
+        ]
+ 
+        settings = {
+            'template_path': 'templates'
+        }
+        tornado.web.Application.__init__(self, handlers, **settings)
+ 
+ 
+if __name__ == '__main__':
+    ws_app = Application()
+    server = tornado.httpserver.HTTPServer(ws_app)
+    server.listen(8080)
+    tornado.ioloop.IOLoop.instance().start()
